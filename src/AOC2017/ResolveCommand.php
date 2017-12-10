3d685f6f04ebe6d3acf2e0670d24ac9f4af05e46
Dominik Voda <d.voda94@gmail.com>
2017-12-10 03:03:08 +0100

    @ resolve options

--- src/AOC2017/ResolveCommand.php
+++ src/AOC2017/ResolveCommand.php
@@ -7,16 +7,21 @@ use Exception;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ResolveCommand extends Command
 {
     private const DAY_ARGUMENT = 'day';
+    private const FIRST_OPTION = 'first';
+    private const SECOND_OPTION = 'second';
 
     public function configure()
     {
         $this->setName('resolve');
         $this->addArgument(self::DAY_ARGUMENT, InputArgument::REQUIRED);
+        $this->addOption(self::FIRST_OPTION, 'f', InputOption::VALUE_NONE);
+        $this->addOption(self::SECOND_OPTION, 's', InputOption::VALUE_NONE);
     }
 
     /**
@@ -30,12 +35,23 @@ class ResolveCommand extends Command
     {
         $dayNumber = $input->getArgument(self::DAY_ARGUMENT);
         $dayClass = sprintf('AOC2017\Days\Day%s', $dayNumber);
+        $first = $input->getOption(self::FIRST_OPTION);
+        $second = $input->getOption(self::SECOND_OPTION);
 
         if (class_exists($dayClass)) {
             /** @var DefaultDay $day */
             $day = new $dayClass();
-            $output->writeln(sprintf('Result 1: %s', $day->getFirstResult($dayNumber)));
-            $output->writeln(sprintf('Result 2: %s', $day->getSecondResult($dayNumber)));
+
+            if (!$first && !$second) {
+                $first = $second = true;
+            }
+
+            if ($first) {
+                $output->writeln(sprintf('Result 1: %s', $day->getFirstResult($dayNumber)));
+            }
+            if ($second) {
+                $output->writeln(sprintf('Result 2: %s', $day->getSecondResult($dayNumber)));
+            }
         } else {
             $output->writeln(sprintf('Class %s not found', $dayClass));
         }
